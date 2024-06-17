efb1ea88c399517f87c2638cd544709a8d2d3516
MarcoSkiadas <Skiadas2@hotmail.com>
2024-06-17 10:45:42 +0200

    Initial commit

---
+++ src/main/java/org/example/asterixapi1706/AsterixApi1706Application.java
@@ -0,0 +1,13 @@
+package org.example.asterixapi1706;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsterixApi1706Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsterixApi1706Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/asterixapi1706/AsterixApi1706ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.asterixapi1706;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsterixApi1706ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
