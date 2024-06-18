16449bce639207689e78a1b03adf6c1f4e346c68
MarcoSkiadas <Skiadas2@hotmail.com>
2024-06-18 15:48:39 +0200

    adding flapdoodle and Tests

---
+++ src/test/java/org/example/asterixapi1706/Controller/AsterixControllerTest.java
@@ -0,0 +1,53 @@
+package org.example.asterixapi1706.Controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AsterixControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void getAllCharacters() {
+    }
+
+    @Test
+    void findCharacters() {
+    }
+
+    @Test
+    void averageAgeByProfessionC() {
+    }
+
+    @Test
+    void addCharacterC() {
+    }
+
+    @Test
+    void updateCharacter() {
+    }
+
+    @Test
+    void deleteCharacter() {
+    }
+
+    @Test
+    void getCharacterById() {
+    }
+
+    @Test
+    void updateCharacterById() {
+    }
+
+    @Test
+    void deleteCharacterById() {
+    }
+}
